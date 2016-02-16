d6a2782c4561c88183ddd2d249d98ed9a6700de9
tanomartin <tanomartin05@gmail.com>
2016-02-16 14:15:26 -0300

    agregado sedes

--- sedes.php
+++ sedes.php
@@ -91,10 +91,17 @@ print_r($grises);*/
 							<iframe width="530" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=es&amp;q=Acceso+A+Rafael+Obligado,+Recoleta,+Buenos+Aires,+Ciudad+Aut%C3%B3noma+de+Buenos+Aires,+Argentina&amp;aq=&amp;sll=-34.572592,-58.314056&amp;sspn=0.114067,0.264187&amp;ie=UTF8&amp;geocode=FVCA8P0dNueE_A&amp;split=0&amp;hq=&amp;hnear=Acceso+A+Rafael+Obligado,+Recoleta,+Buenos+Aires,+Argentina&amp;t=m&amp;ll=-34.568634,-58.394995&amp;spn=0.012722,0.04549&amp;z=14&amp;iwloc=&amp;output=embed"></iframe>
 					</div>
 		        </div> 
-	          	  <div style="float:left; top:-100px; margin-left:48px">
+	          	  <div style="float:left; margin-left:48px; height:750px">
 					  <img src="img/sedes/sedes3.jpg" />
 					  <div style="position:relative; top:-195px; left:115px; border: 3px solid #DD2C8D; border-radius:4px; width:530px">
 							<iframe width="530" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5536.485459706896!2d-58.68211444615104!3d-34.40099133651545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca1129356b343%3A0x3177b9c3445095f2!2sAv.+Benavidez+2400%2C+B1621HZV+Benavidez%2C+Buenos+Aires!5e0!3m2!1ses-419!2sar!4v1449769476829"></iframe>
+					  </div>
+		        </div> 
+		        
+		        <div style="float:left; margin-left:48px; height:750px">
+					  <img src="img/sedes/sedes4.jpg" />
+					  <div style="position:relative; top:-195px; left:115px; border: 3px solid #DD2C8D; border-radius:4px; width:530px">
+							<iframe width="530" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.391009662385!2d-58.45398618494627!3d-34.61955796584305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca22d1b46477%3A0x2b83578deb47f453!2sAv.+Avellaneda+1423%2C+C1405COB+CABA!5e0!3m2!1ses-419!2sar!4v1455642043621"></iframe>
 					  </div>
 		        </div>             
 			</div>
